618a1b6d9cb4a2e569a262f4f2011745bb122756
Oleg Shalygin <oleg.shalygin@amerihome.com>
2016-08-01 11:07:28 -0700

    readme changes and npm task changes for windows environments

--- README.md
+++ README.md
@@ -10,10 +10,28 @@
 3.  Run  `npm start`  to kick off the application.
     * It should automatically load a browser window and point to  `http://localhost:9999`.
     * If the browser window does not load, then manually navigate your browser to  `http://localhost:9999`.
-3.  If you are on a Windows environment, run `npm run start-windows`.
+3.  If you are on a Windows environment, run `npm run start-windows` to launch the application.
     * babel-watch doesn't work on Windows as it uses unix named pipes.
+    * Various other differences between OSX and windows such as setting the node environment variable and path lookup.
 4.  Run `npm test` to explicitly run the tests in the solution.
-    * Note that they are ran upon application start regardless, but this is a quicker feedback loop.
+    * In windows run, `npm run windows-test`.
+5.  The build steps are also available and are in development mode. It is recommended that you run `npm start` to experience HMR and other development level features.  For production deployment builds, the command is `npm build`.
+
+### OSX/Linux
+```sh
+npm install
+npm start
+npm test
+npm build
+```
+
+### Windows
+```sh
+npm install
+npm run windows-start
+npm run windows-test
+npm run windows-build
+```
 
 ####Primary Technology Used in this application:
 * ExpressJs
--- client/components/home/description.jsx
+++ client/components/home/description.jsx
@@ -9,8 +9,14 @@ const Description = () => {
                 <li>Clone this repository</li>
                 <li>Run <kbd>npm install</kbd> to pull in the project dependencies</li>
                 <li>Run <kbd>npm start</kbd> to kick off the application.  It should automatically load a browser window and point to <code>http://localhost:9999</code>.  If the browser window does not load, then manually navigate your browser to <code>http://localhost:9999</code>.</li>
-                <li>If you are on a Windows environment, run <kbd>npm run start-windows -s</kbd>. <code>babel-watch</code> doesn't work on Windows as it uses unix named pipes.</li>
-                <li>Run <kbd>npm test</kbd> to explicitly run the tests in the solution.Note that they are ran upon application start regardless, but this is a quicker feedback loop.</li>
+                <li>If you are on a Windows environment, run <kbd>npm run windows-start -s</kbd>. <code>babel-watch</code> doesn't work on Windows as it uses unix named pipes.</li>
+                <li>Various other differences between OSX and windows such as setting the node environment variable and path lookup.</li>
+                <li>Run <kbd>npm test</kbd> to explicitly run the tests in the solution.Note that they are ran upon application start regardless, but this is a quicker feedback loop.
+                    <ul>
+                        <li>In windows run, <kbd>npm run windows-test.</kbd></li>
+                    </ul>
+                </li>
+                <li>The build steps are also available and are in development mode. It is recommended that you run <kbd>npm start</kbd> to experience HMR and other development level features.  For production deployment builds, the command is <kbd>npm build</kbd>.</li>
             </ol>
             <h4>Primary Technology Used:</h4>
             <ul>
